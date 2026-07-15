b61ac507e4606d1cafb336ca24723de24bbc5957
AbigaelKirwa <abigaelkirwa1937@gmail.com>
2026-07-15 22:45:37 +0300

    fix:removes the availablity import from main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from app.api.routes.patient import router as patient_router
 from app.api.routes.doctor import router as doctor_router
-from app.api.routes.availability import router as availability_router
 
 app = FastAPI(
     title="Clinic Booking App"
@@ -10,7 +9,6 @@ app = FastAPI(
 
 app.include_router(patient_router)
 app.include_router(doctor_router)
-app.include_router(availability_router)
 
 @app.get("/health")
 def health_check():
